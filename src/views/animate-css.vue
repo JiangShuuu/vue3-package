0332b4ed6b83c66ca1d7a8c43e6607fe2ca40831
JiangShuuu <globelex65@gmail.com>
2023-05-03 13:30:39 +0800

    element

--- src/views/animate-css.vue
+++ src/views/animate-css.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>123</p>
+  <h1 class="animate__animated animate__bounce">An animated element</h1>
+  <p class="element01">element01</p>
 </template>
+
+<style lang="postcss" scoped>
+.element01 {
+  display: inline-block;
+  margin: 0 0.5rem;
+
+  animation: bounce; /* referring directly to the animation's @keyframe declaration */
+  animation-duration: 2s; /* don't forget to set a duration! */
+}
+</style>
